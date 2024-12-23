e7699f54e997b73357bee59a1520d37e6f3d3f5a
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-12-23 12:18:07 -0800

    wip

--- builds/cdn.js
+++ builds/cdn.js
@@ -4,5 +4,5 @@
 import plugin from '../src/index.js'
 
 document.addEventListener('alpine:init', () => {
-    window.Alpine.plugin(plugin)
+  window.Alpine.plugin(plugin)
 })
--- scripts/build.js
+++ scripts/build.js
@@ -1,80 +1,80 @@
 // Adapted from the oficial Alpine.js file:
 // https://github.com/alpinejs/alpine/blob/main/scripts/build.js
 
-let fs = require("fs");
-let zlib = require("zlib");
+let fs = require('fs')
+let zlib = require('zlib')
 
-bundle();
+bundle()
 
 function bundle() {
   // Give esbuild specific configurations to build.
 
   // This output file is meant to be loaded in a browser's <script> tag.
   build({
-    entryPoints: ["builds/cdn.js"],
-    outfile: "dist/cdn.js",
+    entryPoints: ['builds/cdn.js'],
+    outfile: 'dist/cdn.js',
     bundle: true,
-    platform: "browser",
-    define: { CDN: "true" },
-  });
+    platform: 'browser',
+    define: { CDN: 'true' },
+  })
 
   // Build a minified version.
   build({
-    entryPoints: ["builds/cdn.js"],
-    outfile: "dist/cdn.min.js",
+    entryPoints: ['builds/cdn.js'],
+    outfile: 'dist/cdn.min.js',
     bundle: true,
     minify: true,
-    platform: "browser",
-    define: { CDN: "true" },
+    platform: 'browser',
+    define: { CDN: 'true' },
   }).then(() => {
-    outputSize("dist/cdn.min.js");
-  });
+    outputSize('dist/cdn.min.js')
+  })
 
   // Then output two files: an esm module and a cjs module.
   // The ESM one is meant for "import" statements (bundlers and new browsers)
   // and the cjs one is meant for "require" statements (node).
   build({
-    entryPoints: ["builds/module.js"],
-    outfile: "dist/module.esm.js",
+    entryPoints: ['builds/module.js'],
+    outfile: 'dist/module.esm.js',
     bundle: true,
-    platform: "neutral",
-    mainFields: ["module", "main"],
-  });
+    platform: 'neutral',
+    mainFields: ['module', 'main'],
+  })
 
   build({
-    entryPoints: ["builds/module.js"],
-    outfile: "dist/module.cjs.js",
+    entryPoints: ['builds/module.js'],
+    outfile: 'dist/module.cjs.js',
     bundle: true,
-    target: ["node10.4"],
-    platform: "node",
-  });
+    target: ['node10.4'],
+    platform: 'node',
+  })
 }
 
 function build(options) {
-  options.define || (options.define = {});
-  options.define["process.env.NODE_ENV"] = process.argv.includes("--watch")
+  options.define || (options.define = {})
+  options.define['process.env.NODE_ENV'] = process.argv.includes('--watch')
     ? `'production'`
-    : `'development'`;
+    : `'development'`
 
-  return require("esbuild")
+  return require('esbuild')
     .build({
-      watch: process.argv.includes("--watch"),
+      watch: process.argv.includes('--watch'),
       // external: ['alpinejs'],
       ...options,
     })
-    .catch(() => process.exit(1));
+    .catch(() => process.exit(1))
 }
 
 function outputSize(file) {
-  let size = bytesToSize(zlib.brotliCompressSync(fs.readFileSync(file)).length);
+  let size = bytesToSize(zlib.brotliCompressSync(fs.readFileSync(file)).length)
 
-  console.log("\x1b[32m", `${file}: ${size}`);
+  console.log('\x1b[32m', `${file}: ${size}`)
 }
 
 function bytesToSize(bytes) {
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-  if (bytes === 0) return "n/a";
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-  if (i === 0) return `${bytes} ${sizes[i]}`;
-  return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes === 0) return 'n/a'
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
+  if (i === 0) return `${bytes} ${sizes[i]}`
+  return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
 }
